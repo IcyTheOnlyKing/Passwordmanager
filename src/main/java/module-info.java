45ab8600fdda91b7f05eb0874d3f1a6a529d5ab6
Dendoxyn <dkimmers@htl-steyr.ac.at>
2025-10-23 09:19:33 +0200

    module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,17 @@
 module htl.steyr.passwordmanager {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
+    requires javafx.graphics;
 
-    requires org.controlsfx.controls;
+    // falls du weitere Libs nutzt:
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires eu.hansolo.toolboxfx;
 
-    opens htl.steyr.passwordmanager to javafx.fxml;
+    // 👇 Das hier ist entscheidend:
+    opens htl.steyr.passwordmanager.controller to javafx.fxml;
+
+    // optional, wenn du mehrere Packages hast:
     exports htl.steyr.passwordmanager;
 }
